737ffa925e81607e9fc928833c646b912b55f9ef
Boris Bonett <borisbonett@MacBook-Pro-de-Boris.local>
2026-05-01 17:45:36 -0500

    ui dashboard in progress

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,7 +3,13 @@ import { Routes } from '@angular/router';
 export const routes: Routes = [
     {
         path: 'dashboard',
-        loadComponent: () => import('./pages/dashboard-page/dashboard-page.component')
+        loadComponent: () => import('./pages/dashboard-page/dashboard-page.component'),
+        children: [
+            {
+                path: 'search',
+                loadComponent: () => import('./pages/search-page/search-page.component')
+            },
+        ]
     },
     {
         path: '**',
---
+++ src/app/pages/search-page/search-page.component.ts
@@ -0,0 +1,79 @@
+import { Component, inject, signal } from "@angular/core";
+import { PokeApiService } from "../../services/poke_api.services";
+import { forkJoin } from "rxjs";
+import { switchMap } from "rxjs/operators";
+
+@Component({
+    selector: "app-dashboard-page",
+    templateUrl: "./search-page.component.html",
+    styleUrls: ['./search-page.component.scss'],
+    imports: [],
+})
+export default class SearchPageComponent {
+
+    private pokeApiService = inject(PokeApiService);
+
+    pokemons = signal<any[]>([]);
+    isLoading = signal<boolean>(false);
+    offset = signal<number>(0);
+    limit = 20;
+
+    typeColors: { [key: string]: string } = {
+        normal: '#A8A77A',
+        fire: '#EE8130',
+        water: '#6390F0',
+        electric: '#F7D02C',
+        grass: '#7AC74C',
+        ice: '#96D9D6',
+        fighting: '#C22E28',
+        poison: '#A33EA1',
+        ground: '#E2BF65',
+        flying: '#A98FF3',
+        psychic: '#F95587',
+        bug: '#A6B91A',
+        rock: '#B6A136',
+        ghost: '#735797',
+        dragon: '#6F35FC',
+        dark: '#705746',
+        steel: '#B7B7CE',
+        fairy: '#D685AD',
+    };
+
+    ngOnInit() {
+        this.loadPokemonPage();
+    }
+
+    loadPokemonPage() {
+        this.isLoading.set(true);
+        this.pokemons.set([]); 
+        
+        this.pokeApiService.getAllPokemon(this.offset(), this.limit).pipe(
+            switchMap((data: any) => {
+                const requests = data.results.map((pokemon: any) => 
+                    this.pokeApiService.getPokemonByUrl(pokemon.url)
+                );
+                return forkJoin(requests);
+            })
+        ).subscribe((pokemonDetails: any) => {
+            this.pokemons.set(pokemonDetails);
+            this.isLoading.set(false);
+        });
+    }
+
+    nextPage() {
+        this.offset.update(v => v + this.limit);
+        this.loadPokemonPage();
+    }
+
+    prevPage() {
+        if (this.offset() >= this.limit) {
+            this.offset.update(v => v - this.limit);
+            this.loadPokemonPage();
+        }
+    }
+
+    getTypeColor(type: string): string {
+        return this.typeColors[type] || '#777';
+    }
+
+}
